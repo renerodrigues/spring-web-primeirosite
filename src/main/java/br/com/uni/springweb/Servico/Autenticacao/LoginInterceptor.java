793b467b7e2ed0c1c4a651c08a08ff0bd06d7898
Rene Rodrigues <91230707+renerodrigues@users.noreply.github.com>
2022-11-02 20:34:48 -0300

    atualização de propriedades da aplicação - teste

--- src/main/java/br/com/uni/springweb/Servico/Autenticacao/LoginInterceptor.java
+++ src/main/java/br/com/uni/springweb/Servico/Autenticacao/LoginInterceptor.java
@@ -25,14 +25,14 @@ public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
             @Nullable Exception ex) throws Exception {
-        // TODO Auto-generated method stub
+
         HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
             @Nullable ModelAndView modelAndView) throws Exception {
-        // TODO Auto-generated method stub
+
         HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
     }
 
